f20002e995e6ef23e6a2e623e90a285e45f97fa7
baraksu <baraksu@gmail.com>
2015-06-23 10:17:25 +0300

    Reverse String

---
+++ ExerciseTester/ReverseStringTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JobTester;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExerciseTester
+{
+    [TestClass]
+    public class ReverseStringTester
+    {
+        [TestMethod]
+        public void Reverse_EmptyString_EmptyString()
+        {
+            ReverseString rs = new ReverseString();
+
+            string reverse = rs.Reverse(" 01234");
+
+            Assert.AreEqual(reverse, "43210 ");
+        }
+           [TestMethod]
+        public void Reverse_Regular_ReverseString()
+        {
+            ReverseString rs = new ReverseString();
+
+            string reverse = rs.Reverse(String.Empty);
+
+            Assert.AreEqual(reverse, String.Empty);
+        }
+    }
+}
---
+++ JobTester/ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTester
+{
+    public class ReverseString
+    {
+        public string Reverse(string s)
+        {
+            if (s == null)
+                return null;
+            if (s.Length == 0)
+                return String.Empty;
+
+
+
+            char[] r = new char[s.Length]; { }
+            for (int i = 0; i < s.Length; i++)
+            {
+                r[i] = s[s.Length - i - 1];
+            }
+
+            string result = new string(r);
+            return result;
+
+        }
+    }
+}
